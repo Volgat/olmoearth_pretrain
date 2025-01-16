b87c2c29e0adfcf8d301135cc2378aea20c9f076
hgherzog <henryh@allenai.org>
2025-01-16 14:01:32 -0800

    initial test

--- tests/unit/data/test_index.py
+++ tests/unit/data/test_index.py
@@ -14,9 +14,17 @@ def sample_index_path() -> str:
 def test_dataset_index(sample_index_path: str) -> None:
     """Test the dataset index."""
     index_parser = DatasetIndexParser(sample_index_path)
-    assert len(index_parser.samples) == 6
+    assert len(index_parser.samples) == 4
     assert set(index_parser.samples[0].keys()) == {
         "data_source_paths",
         "data_source_metadata",
         "sample_metadata",
     }
+    expected_paths = {
+        "sentinel2": "tests/fixtures/sample-dataset/sentinel2_monthly/example_001.tif"
+    }
+    actual_paths = index_parser.samples[0]["data_source_paths"]
+    assert len(expected_paths) == len(actual_paths)
+    for data_source, expected_path in expected_paths.items():
+        assert data_source in actual_paths
+        assert str(actual_paths[data_source]) == expected_path
