652a88934d0fcf6a0f3c4f128bda0e8e280c9b54
Joseph Redmon <pjreddie@gmail.com>
2025-04-09 12:02:18 -0700

    adding to supported clusters

--- helios/internal/common.py
+++ helios/internal/common.py
@@ -23,7 +23,7 @@ WORKSPACE = "ai2/earth-systems"
 DEFAULT_HELIOS_WEKA_BUCKET = BeakerWekaBucket("dfive-default", "/weka/dfive-default")
 PROJECT_NAME = "helios"
 
-WEKA_CLUSTER_NAMES = ["jupiter", "saturn", "neptune", "ceres", "triton"]
+WEKA_CLUSTER_NAMES = ["jupiter", "saturn", "neptune", "ceres", "triton", "titan"]
 
 
 def get_root_dir(cluster: str) -> str:
