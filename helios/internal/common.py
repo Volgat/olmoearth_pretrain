89925d8d038c35a4df3eceb535542e6775ebf88f
Joseph Redmon <pjreddie@gmail.com>
2025-05-19 12:43:36 -0700

    normies

--- helios/internal/common.py
+++ helios/internal/common.py
@@ -116,7 +116,7 @@ def build_launch_config(
         shared_memory="256GiB",
         shared_filesystem=True,  # We only use Weka for now
         allow_dirty=False,
-        priority=BeakerPriority.high,
+        priority=BeakerPriority.normal,
         env_vars=[
             BeakerEnvVar(name="NCCL_DEBUG", value="INFO" if nccl_debug else "WARN"),
             BeakerEnvVar(
