94ab596f0774c55d0289a3f4c8714a19fe5575e4
Yawen Zhang <yawenz@allenai.org>
2025-06-05 17:35:31 -0700

    add val script

--- scripts/v0_sweep/v0.1_sweep_eval.py
+++ scripts/v0_sweep/v0.1_sweep_eval.py
@@ -32,8 +32,6 @@ for lr in LP_LRs:
     print(f"Running: {full_cmd}")
     subprocess.run(full_cmd, shell=True)  # nosec
 
-exit(0)
-
 # # v0.1_base_latent_mim_space_time
 # python scripts/v0_sweep/latent_mim.py launch v0.1_base_latent_mim_space_time ai2/titan-cirrascale --model.decoder_config.depth=4 --common.launch.num_gpus=1 --train_module.masking_config.strategy_config.type=space_time --model.reconstructor_config=null --train_module.mae_loss_config=null --train_module.ema_decay=\[1,1\] --trainer.load_path=/weka/dfive-default/helios/checkpoints/joer/v0.1_base_latent_mim_space_time/step165000
 
