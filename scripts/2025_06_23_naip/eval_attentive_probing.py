629653bdc42ece03fd89178edb4fcdc4fbc1059b
hgherzog <henryh@allenai.org>
2025-07-09 11:44:45 -0700

    try to launch probe in a configurable way

--- scripts/2025_06_23_naip/eval_attentive_probing.py
+++ scripts/2025_06_23_naip/eval_attentive_probing.py
@@ -1,11 +1,15 @@
 """Run sweep over learning rates."""
 
-import subprocess  # nosec
 import argparse
+import subprocess  # nosec
+
 
 def main():
+    """Run sweep over learning rates."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cluster", type=str, default="ai2/titan-cirrascale", help="Cluster to run on")
+    parser.add_argument(
+        "--cluster", type=str, default="ai2/titan-cirrascale", help="Cluster to run on"
+    )
     args = parser.parse_args()
 
     LP_LRs = [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
@@ -22,6 +26,16 @@ def main():
         "--trainer.callbacks.downstream_evaluator.tasks.sickle_landsat.probe_lr={lr}",
         "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1_landsat.probe_lr={lr}",
         "--trainer.callbacks.downstream_evaluator.tasks.breizhcrops.probe_lr={lr}",
+        "--trainer.callbacks.downstream_evaluator.tasks.m-sa-crop-type.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.m-cashew-plant.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.mados.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.sen1floods11.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel2.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.pastis_sentinel1_sentinel2.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_landsat.probe_type=attnpool",
+        "--trainer.callbacks.downstream_evaluator.tasks.sickle_sentinel1_landsat.probe_type=attnpool",
     ]
 
     for probe_lr in LP_LRs:
@@ -48,36 +62,37 @@ def main():
         #     ]
         #     + [arg.format(lr=probe_lr) for arg in lr_args],
         # )  # nosec
-        # subprocess.call(
-        #     [
-        #         "python",
-        #         "scripts/2025_06_23_naip/eval.py",
-        #         "launch",
-        #         f"v0.2_small_latent_mim_128_naip_moredata_random_fixed_modality_0.5_nonaip_eval_{probe_lr}",
-        #         args.cluster,
-        #         "--trainer.load_path=/weka/dfive-default/helios/checkpoints/favyen/v0.2_small_latent_mim_128_naip_moredata_random_fixed_modality_0.5_nonaip/step310000",
-        #         "--common.training_modalities=[sentinel2_l2a,sentinel1,worldcover,latlon,srtm,landsat,openstreetmap_raster]",
-        #         "--model.encoder_config.embedding_size=384",
-        #         "--model.decoder_config.encoder_embedding_size=384",
-        #         "--model.decoder_config.decoder_embedding_size=384",
-        #         "--model.encoder_config.num_heads=6",
-        #         "--model.decoder_config.num_heads=6",
-        #     ]
-        #     + [arg.format(lr=probe_lr) for arg in lr_args],
-        # )  # nosec
         subprocess.call(
             [
                 "python",
-                "scripts/2025_06_23_naip/eval_alldata.py",
+                "scripts/2025_06_23_naip/eval.py",
                 "launch",
-                f"v0.2_base_latent_mim_128_alldata_random_fixed_modality_0.5_attn_probe_eval_{probe_lr}",
+                f"v0.2_small_latent_mim_128_naip_moredata_random_fixed_modality_0.5_nonaip_eval_{probe_lr}",
                 args.cluster,
-                "--trainer.load_path=/weka/dfive-default/helios/checkpoints/favyen/v0.2_base_latent_mim_128_alldata_random_fixed_modality_0.5/step320000", #best
+                "--trainer.load_path=/weka/dfive-default/helios/checkpoints/favyen/v0.2_small_latent_mim_128_naip_moredata_random_fixed_modality_0.5_nonaip/step310000",
                 "--common.training_modalities=[sentinel2_l2a,sentinel1,worldcover,latlon,srtm,landsat,openstreetmap_raster]",
-                "--launch.priority=normal"
+                "--model.encoder_config.embedding_size=384",
+                "--model.decoder_config.encoder_embedding_size=384",
+                "--model.decoder_config.decoder_embedding_size=384",
+                "--model.encoder_config.num_heads=6",
+                "--model.decoder_config.num_heads=6",
             ]
             + [arg.format(lr=probe_lr) for arg in lr_args],
         )  # nosec
+        # subprocess.call(
+        #     [
+        #         "python",
+        #         "scripts/2025_06_23_naip/eval_alldata.py",
+        #         "launch",
+        #         f"v0.2_base_latent_mim_128_alldata_random_fixed_modality_0.5_attn_probe_eval_{probe_lr}",
+        #         args.cluster,
+        #         "--trainer.load_path=/weka/dfive-default/helios/checkpoints/favyen/v0.2_base_latent_mim_128_alldata_random_fixed_modality_0.5/step320000", #best
+        #         "--common.training_modalities=[sentinel2_l2a,sentinel1,worldcover,latlon,srtm,landsat,openstreetmap_raster]",
+        #         "--launch.priority=normal"
+        #     ]
+        #     + [arg.format(lr=probe_lr) for arg in lr_args],
+        # )  # nosec
+
 
 if __name__ == "__main__":
     main()
