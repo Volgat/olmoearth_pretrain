93b84715c7f37392cd43ced1ae14472138654901
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-07-29 11:00:50 +0100

    norm_stats_from_pretrained=True

--- scripts/2025_06_26_dataset_percentage_experiments/eval.py
+++ scripts/2025_06_26_dataset_percentage_experiments/eval.py
@@ -64,7 +64,7 @@ def build_trainer_config(common: CommonComponents) -> TrainerConfig:
             embedding_batch_size=128,
             num_workers=8,
             pooling_type=PoolingType.MEAN,
-            norm_stats_from_pretrained=False,
+            norm_stats_from_pretrained=True,
             eval_interval=Duration.epochs(1),
         ),
         "m-eurosat": DownstreamTaskConfig(
