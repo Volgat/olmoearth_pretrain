4c66dd530882b804c89828df9b6795f7fbc55db6
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-09-18 08:19:38 +0100

    wip

--- helios/evals/models/prithvi/prithvi.py
+++ helios/evals/models/prithvi/prithvi.py
@@ -14,20 +14,22 @@ from timm.layers import to_2tuple
 from timm.models.vision_transformer import Block
 from helios.evals.models.prithvi.prithvi_mae import PrithviMAE
 from helios.data.constants import Modality
+from helios.nn.flexihelios import PoolingType
+from helios.train.masking import MaskedHeliosSample
 
 
 class PrithviWrapper(nn.Module):
 
-    def __init__(self, weights_path: Path, do_pool=True, temporal_pooling: str = "mean"):
+    def __init__(self, weights_path: Path,):
         super().__init__()
 
-        with (weights_path / "prithvi/config.json").open("r") as f:
+        with (weights_path / "config.json").open("r") as f:
             config = yaml.safe_load(f)["pretrained_cfg"]
 
         config["num_frames"] = 1
 
         self.model = PrithviMAE(**config)
-        state_dict = torch.load(weights_path / "prithvi/Prithvi_EO_V2_300M.pt", map_location="cpu")
+        state_dict = torch.load(weights_path / "Prithvi_EO_V2_300M.pt", map_location="cpu")
         # discard fixed pos_embedding weight, following
         # https://huggingface.co/ibm-nasa-geospatial/Prithvi-EO-2.0-300M/blob/e4aabdc440c8ee703a749def8af5bf4700dee35b/inference.py#L362
         for k in list(state_dict.keys()):
@@ -35,38 +37,91 @@ class PrithviWrapper(nn.Module):
                 del state_dict[k]
         self.model.load_state_dict(state_dict, strict=False)
         self.image_resolution = config["img_size"]
-        self.grid_size = int(config["img_size"] // config["patch_size"][-1])
         self.bands = config["bands"]
-
         self.inputs_to_prithvi = [Modality.SENTINEL2_L2A.band_order.index(b) for b in self.bands]
-        self.do_pool = do_pool
-        if temporal_pooling not in ["mean", "max"]:
-            raise ValueError(
-                f"Expected temporal_pooling to be in ['mean', 'max'], got {temporal_pooling}"
+
+    def _process_modality_data(
+        self, data: torch.Tensor, modality: str
+    ) -> list[torch.Tensor]:
+        """Process individual modality data.
+
+        Args:
+            data: Input tensor of shape [B, H, W, T, C]
+            modality: What modality data is
+
+        Returns:
+            list of tensors of shape [B, C, H, W]
+        """
+        t_dim = data.shape[3]
+
+        # Get original dimensions
+        original_height = data.shape[2]
+        data_list = []
+
+        for i in range(t_dim):
+            data_i = rearrange(data[:, :, :, i, :], "b h w c -> b c h w")
+            data_i = data_i[:, HELIOS_TO_SATLAS[modality], :, :]
+
+            new_height = (
+                self.patch_size if original_height == 1 else self.image_resolution
             )
-        self.temporal_pooling = temporal_pooling
-        self.dim = config["embed_dim"]
 
-    def resize(self, images):
-        images = F.interpolate(
-            images,
-            size=(self.image_resolution, self.image_resolution),
-            mode="bilinear",
-            align_corners=False,
-        )
-        return images
+            data_i = F.interpolate(
+                data_i,
+                size=(new_height, new_height),
+                mode="bilinear",
+                align_corners=False,
+            )
+            if self.use_pretrained_normalizer:
+                data_i = self.normalize(data_i, modality)
+            data_list.append(data_i)
 
-    def preproccess(self, images):
-        if len(images.shape) == 5:
-            # take the mean along the temporal dimension
-            images = torch.mean(images, dim=2)
-        images = rearrange(images, "b h w c -> b c h w")
-        assert images.shape[1] == 13
-        images = images[:, self.inputs_to_prithvi, :, :]
-        images = self.resize(images)  # (bsz, C, H, W)
-        return repeat(images, "b c h w -> b c t h w", t=1)
+        return data_list
 
-    def forward(self, s2=None, s1=None, months=None):
+    def prepare_input(
+        self,
+        masked_helios_sample: MaskedHeliosSample,
+    ) -> tuple[list[torch.Tensor], str]:
+        """Prepare input for the Prithvi model from MaskedHeliosSample."""
+        if len(masked_helios_sample.modalities) != 1:
+            raise RuntimeError(
+                f"Prithvi only supports one modality. Received {len(masked_helios_sample.modalities)}: {masked_helios_sample.modalities}"
+            )
+        modality = masked_helios_sample.modalities[0]
+        if modality != Modality.SENTINEL2_L2A.name:
+            raise RuntimeError(f"Prithvi only supports Sentinel2_L2A. Received {modality}")
+
+        data = getattr(masked_helios_sample, modality)
+        return self._process_modality_data(data, modality), modality
+
+    def forward(
+        self,
+        masked_helios_sample: MaskedHeliosSample,
+        pooling: PoolingType = PoolingType.MEAN,
+        spatial_pool: bool = False,
+    ) -> torch.Tensor:
+        """Forward pass through the satlas model."""
+        processed_inputs, modality = self.prepare_input(masked_helios_sample)
+        outputs_list: list[torch.Tensor] = []
+        for per_t_input in processed_inputs:
+            output = self.model(per_t_input)[-1]
+            # output shape for atto: (bsz, 320, 7, 7)
+            # output shape for tiny: (bsz, 768, 6, 6)
+            if not spatial_pool:
+                # then we don't want to keep the spatial dimensions
+                output = output.mean(dim=-1).mean(dim=-1)
+            else:
+                output = rearrange(output, "b c h w -> b h w c")
+            outputs_list.append(output.unsqueeze(0))
+
+        # stack in the timestep dimension and take the mean or maybe the max?
+        if pooling == PoolingType.MEAN:
+            output_features = torch.cat(outputs_list, dim=0).mean(dim=0)
+        elif pooling == PoolingType.MAX:
+            output_features = torch.max(torch.cat(outputs_list, dim=0), dim=0)[0]
+        return output_features
+
+    def forward(self, s2=None,  do_pool=True, temporal_pooling: str = "mean"):
         if s2 is None:
             raise ValueError("S2 can't be None for Prithvi")
 
