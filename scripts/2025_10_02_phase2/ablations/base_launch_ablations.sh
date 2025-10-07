a52217d1cb73ac66c4cb61b15a3cbc4f9220f84c
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2025-10-07 11:12:31 +0100

    No maps + launch

--- scripts/2025_10_02_phase2/ablations/base_launch_ablations.sh
+++ scripts/2025_10_02_phase2/ablations/base_launch_ablations.sh
@@ -4,3 +4,7 @@ python scripts/2025_10_02_phase2/base.py launch phase2.0_base_no_contrastive ai2
 python scripts/2025_10_02_phase2/base.py launch phase2.0_base_random_masking ai2/ceres-cirrascale  --train_module.masking_config.strategy_config="{'type': 'random', 'encode_ratio': 0.5, 'decode_ratio': 0.5}" --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
 # MAE
 python scripts/2025_10_02_phase2/ablations/base_mae.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
+# no ag maps (removing worldcereal and CDL)
+python scripts/2025_10_02_phase2/ablations/no_ag_maps.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
+# no maps (removing ag maps + worldcover, openstreetmap, canopy height)
+python scripts/2025_10_02_phase2/ablations/no_maps.py phase2.0_base_mae ai2/ceres-cirrascale  --launch.clusters='[ai2/jupiter-cirrascale-2]' --launch.priority=high
