Fix masking

helios/train/masking.py
@@ -1127,20 +1127,23 @@ class ModalityCrossMaskingStrategy(MaskingStrategy):
         no_encoded_indices = torch.argwhere(num_encoded == 0)
         no_decoded_indices = torch.argwhere(num_decoded == 0)
         for i in no_encoded_indices:
-            for _, val in masked_batch_dict.items():
-                modality_index = val[i]
-                # will this happen inplace?
-                modality_index[
-                    modality_index == MaskValue.TARGET_ENCODER_ONLY.value
-                ] = MaskValue.ONLINE_ENCODER.value
+            for key, val in masked_batch_dict.items():
+                if key.endswith("mask"):
+                    modality_index = val[i]
+                    # will this happen inplace?
+                    modality_index[
+                        modality_index == MaskValue.TARGET_ENCODER_ONLY.value
+                    ] = MaskValue.ONLINE_ENCODER.value
+                    masked_batch_dict[key][i] = modality_index
         for i in no_decoded_indices:
             for key, val in masked_batch_dict.items():
-                modality_index = val[i]
-                # will this happen inplace?
-                modality_index[
-                    modality_index == MaskValue.TARGET_ENCODER_ONLY.value
-                ] = MaskValue.DECODER.value
-                masked_batch_dict[key][i] = modality_index
+                if key.endswith("mask"):
+                    modality_index = val[i]
+                    # will this happen inplace?
+                    modality_index[
+                        modality_index == MaskValue.TARGET_ENCODER_ONLY.value
+                    ] = MaskValue.DECODER.value
+                    masked_batch_dict[key][i] = modality_index
         masked_batch = MaskedHeliosSample(**masked_batch_dict)
 
         return masked_batch

tests/unit/train/test_masking.py
@@ -1160,25 +1160,28 @@ def test_modality_cross_random_masking() -> None:
 
 def test_modality_cross_random_masking_has_online_encoder_and_decoder_tokens() -> None:
     """Test modality cross random masking."""
-    b, h, w, t = 4, 1, 1, 3
-
-    patch_size = 1
-
-    days = torch.randint(1, 31, (b, 1, t), dtype=torch.long)
-    months = torch.randint(1, 13, (b, 1, t), dtype=torch.long)
-    years = torch.randint(2018, 2020, (b, 1, t), dtype=torch.long)
-    timestamps = torch.cat([days, months, years], dim=1)  # Shape: (B, 3, T)
-    batch = HeliosSample(
-        sentinel2_l2a=torch.ones((b, h, w, t, Modality.SENTINEL2_L2A.num_bands)),
-        timestamps=timestamps,
-    )
-
     masking_strategy = ModalityCrossRandomMaskingStrategy(
         encode_ratio=0.5,
         decode_ratio=0.5,
         allow_encoding_decoding_same_bandset=True,
     )
-    for i in range(100):
+
+    for _ in range(100):
+        b, h_w, t = 100, 1, 1
+
+        patch_size = 1
+
+        days = torch.randint(1, 31, (b, 1, t), dtype=torch.long)
+        months = torch.randint(1, 13, (b, 1, t), dtype=torch.long)
+        years = torch.randint(2018, 2020, (b, 1, t), dtype=torch.long)
+        timestamps = torch.cat([days, months, years], dim=1)  # Shape: (B, 3, T)
+        batch = HeliosSample(
+            sentinel2_l2a=torch.ones(
+                (b, h_w, h_w, t, Modality.SENTINEL2_L2A.num_bands)
+            ),
+            timestamps=timestamps,
+        )
+
         masked_sample = masking_strategy.apply_mask(batch, patch_size=patch_size)
         logger.info(f"masked_sample: {masked_sample.sentinel2_l2a_mask}")
         num_encoded = torch.sum(
@@ -1189,6 +1192,15 @@ def test_modality_cross_random_masking_has_online_encoder_and_decoder_tokens() -
             masked_sample.sentinel2_l2a_mask == MaskValue.DECODER.value,
             dim=(1, 2, 3, 4),
         )
+        print(num_encoded)
+        print(num_decoded)
+        print(
+            torch.sum(
+                masked_sample.sentinel2_l2a_mask == MaskValue.TARGET_ENCODER_ONLY.value,
+                dim=(1, 2, 3, 4),
+            )
+        )
+        assert False
         assert (num_encoded > 0).all()
         assert (num_decoded > 0).all()
 